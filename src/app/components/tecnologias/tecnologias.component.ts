a84391875361bfde26fd560d3a0645a1b4d0a355
Noah <Abbeldn_07@hotmail.com>
2020-12-30 04:57:04 -0600

    Continuando con Tecnologias

--- src/app/components/tecnologias/tecnologias.component.ts
+++ src/app/components/tecnologias/tecnologias.component.ts
@@ -14,10 +14,32 @@ export class TecnologiasComponent implements OnInit {
 
   tecnologias = [
     {
-      name:"CSS",
-      vrsion:"3",
-      logo:"../../../assets/css3.png"
+      name:"Desarrollo Web",
+      herramientas:[
+        {
+          name:"CSS",
+          version:"3",
+          logo:"../../../assets/css3.png",
+          color:"primary",
+          value:"85"
+        },
+        {
+          name:"JavaScript",
+          version:"ECMAScript 6",
+          logo:"../../../assets/javascript.png",
+          color:"warn",
+          value:"60"
+        },
+        {
+          name:"HTML",
+          version:"5",
+          logo:"../../../assets/html5.png",
+          color:"warn",
+          value:"90"
+        },
+      ]
     }
+    
   ]
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,13 +4,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 const MaterialComponents=[
   MatInputModule,
   MatFormFieldModule,
   MatButtonModule,
   MatTooltipModule,
-  MatCardModule
+  MatCardModule,
+  MatProgressBarModule
 ];
 
 
